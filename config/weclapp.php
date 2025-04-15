13247a6a2dc9d50a1a0166a7c67d59896801d575
devgio81 <37338914+devgio81@users.noreply.github.com>
2025-04-15 12:17:13 +0200

    Update weclapp.php
    removed Token

--- config/weclapp.php
+++ config/weclapp.php
@@ -4,6 +4,6 @@ return [
     /*Einstellungen für die Verbindung zur Marketplace-API*/
 
     'base_url' => env('WECLAPP_BASE_URL', 'https://hbtestmarketplace.weclapp.com/webapp/api/v1'),
-    'api_token' => env('WECLAPP_API_TOKEN', 'fab36818-56d8-455a-bfdc-536bb88b8b7d'),
+    'api_token' => env('WECLAPP_API_TOKEN', ''),
     'rate_limit_per_minute' => env('WECLAPP_RATE_LIMIT_PER_MINUTE', 30),
 ];
